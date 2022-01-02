89df31eea3f700402e9405df619329472dbce56a
Anusha Tuladhar <atuladhar@ucdavis.edu>
2022-01-01 22:07:24 -0800

    remove the select dropdown on update

--- frontend/components/basket/edit_basket_item_container.jsx
+++ frontend/components/basket/edit_basket_item_container.jsx
@@ -61,22 +61,11 @@ class EditBasketItemForm extends React.Component{
                   <div className="quantity-price-amount">
                     <div className="quantity-edit">
                       <label className="label-quantity">
-                        <select
+                        <input
                           type="number"
                           value={this.state.quantity}
                           onChange={this.update("quantity")}
-                        >
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                          <option value="6">6</option>
-                          <option value="7">7</option>
-                          <option value="8">8</option>
-                          <option value="9">9</option>
-                          <option value="10">10</option>
-                        </select>
+                        />
                       </label>
                     </div>
                     <div className="subtotal-delete">
