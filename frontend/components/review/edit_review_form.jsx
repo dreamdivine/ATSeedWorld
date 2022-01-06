d3c243ebf8685c3903457ad71c548ddd33f4c7c1
Anusha Tuladhar <atuladhar@ucdavis.edu>
2022-01-06 15:58:00 -0800

    edit container and edit form working on it

--- frontend/components/review/edit_review_form.jsx
+++ frontend/components/review/edit_review_form.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
 class EditReviewForm extends React.Component{
     constructor(props){
         super(props)
+         let reviewLength = 0;
+         let listingReview = [];
+         for (let i = 0; i < this.props.reviews.length; i++) {
+           if (this.props.listing.id === this.props.reviews[i].listing_id) {
+             reviewLength += 1;
+             listingReview.push(reviews[i]);
+           }
+         }
+
+         for (let j = 0; j < listingReview.length; j++){
+             if (listingReview[i].user_id === this.props.currentUserId){
+                 this.state = this.props.reviews[i];
+             }
+         }
     }
 
+    
+   
 
 }
 
