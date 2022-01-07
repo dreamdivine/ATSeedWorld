bef6483f9efb0c47dccf78d71ac87b95cb794c1a
Anusha Tuladhar <atuladhar@ucdavis.edu>
2022-01-07 12:40:24 -0800

    change the position of the middle section f the review

--- frontend/components/review/edit_review_container.js
+++ frontend/components/review/edit_review_container.js
@@ -1,9 +1,13 @@
 import React from "react";
 import {connect} from "react-redux";
-import { updateReview, clearReviewErrors } from "../../actions/review_actions";
+import { updateReview, clearReviewErrors, fetchReviews } from "../../actions/review_actions";
 import ReviewForm from "./review_form";
 
 class EditReviewForm extends React.Component {
+    componentDidMount(){
+        this.props.fetchReviews()
+    }
+
     render(){
         const { currentUser, review, action, formType, clearReviewErrors, handleEdit, listing, errors } =
           this.props;
@@ -35,10 +39,11 @@ const mSTP = (state, ownProps) => {
     };
 }
 
-const mDTP = dispatch => {
+const mDTP = (dispatch, ownProps) => {
     return {
       action: (review) => dispatch(updateReview(review)),
       clearReviewErrors: () => dispatch(clearReviewErrors()),
+      fetchReviews: () => dispatch(fetchReviews())
     };
 }
 
